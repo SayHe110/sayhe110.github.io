74e5cf98ad0fe0d33bd41f90fbef20ed2e33bbcd
LeiJian <949426374@qq.com>
2019-08-15 18:46:25 +0800

    Site updated: 2019-08-15 18:46:25

---
+++ js/baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?7706e2ba8c658f563720bb36e8a87962";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
